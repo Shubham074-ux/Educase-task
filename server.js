34a3590d450506582b20f79d36467413c45ebf4d
Shubham074-ux <127836322+Shubham074-ux@users.noreply.github.com>
2025-04-23 16:43:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,12 +6,7 @@ app.use(express.json());
 
 const dbURL = `${ process.env.MYSQL_URL }`
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+const db = mysql.createConnection(dbURL);
 
 db.connect((err) => {
   if (err) throw err;
